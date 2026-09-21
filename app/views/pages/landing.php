c17a7c4f0c264988e237d793889957f5f5b6b4c6
eban29 <navihistorillo@gmail.com>
2026-09-21 19:50:06 +0800

    Create landing.php

---
+++ app/views/pages/landing.php
@@ -0,0 +1,5 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?>
+    <h1>Welcome to the POS System</h1>
+    <p>A simple Point-of-Sale system built with CodeIgniter 4.</p>
+<?= $this->endSection() ?>
