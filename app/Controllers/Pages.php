a2b94d45138426b2b72fcb14468a312820128975
eban29 <navihistorillo@gmail.com>
2026-09-21 19:48:29 +0800

    Create Pages.php

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('pages/landing', ['title' => 'Home']);
+    }
+
+    public function about()
+    {
+        return view('pages/about', ['title' => 'About']);
+    }
+}
